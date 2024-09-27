70ee68355fc5b681e71eb0c2606e5954c8f0c0b0
JShoon <jsh021813@gmail.com>
2024-09-27 11:43:02 +0900

    feat: prettier 컨벤션 설정

---
+++ prettier.config.cjs
@@ -0,0 +1,12 @@
+// prettier.config.js
+module.exports = {
+	semi: true, // 세미콜론 사용
+	singleQuote: true, // 작은 따옴표 사용
+	trailingComma: 'es5', // ES5 문법에 맞는 곳에만 trailing comma
+	tabWidth: 2, // 탭 너비 2칸
+	useTabs: true, // 탭으로 들여쓰기
+	printWidth: 80, // 줄 길이 80자로 제한
+	bracketSpacing: true, // 객체 리터럴에서 괄호 사이에 공백 넣기
+	jsxSingleQuote: false, // JSX에서는 큰따옴표 사용
+	arrowParens: 'always', // 화살표 함수에서 매개변수에 괄호 항상 사용
+};
