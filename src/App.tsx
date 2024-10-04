355b935101ddb61a990b8424f5ab777366ce7944
JShoon <jsh021813@gmail.com>
2024-10-04 10:02:10 +0900

    fix: 파일 구조 변경

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import './App.css';
-import Login from './pages/login/Login'; // Login 컴포넌트의 실제 경로에 맞게 수정하세요
+import Login from './pages/auth/login/Login'; // Login 컴포넌트의 실제 경로에 맞게 수정하세요
 
 function App() {
 	return (
