79809799453cfd77f742f322a5bf36937ba29630
JShoon <jsh021813@gmail.com>
2024-10-04 09:56:50 +0900

    fix: 파일구조 변경
    - 모든 pr merge 후 파일 구조 변경
    - jsx tsx로 변경 후 타입 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
 import { useState } from 'react';
 import './App.css';
-import Login from './pages/Login'; // Login 컴포넌트의 실제 경로에 맞게 수정하세요
+import Login from './pages/login/Login'; // Login 컴포넌트의 실제 경로에 맞게 수정하세요
 
 function App() {
-  return (
-    <>
-      <Login />
-    </>
-  );
+	return (
+		<>
+			<Login />
+		</>
+	);
 }
 
 export default App;
