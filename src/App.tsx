5eaf4eef5908230a335e6bc64e2acb894531de78
Forhye <onlyh4425@gmail.com>
2024-10-08 10:27:44 +0900

    feat : route 세팅 - main.tsx : 히스토리가 쌓이는 browser router로 app을 감싸줌 - App.tsx : 각 페이지 라우트 세팅

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,18 @@
-import { useState } from 'react';
 import './App.css';
+import { Routes, Route } from 'react-router-dom';
 import Login from './pages/auth/login/Login'; // Login 컴포넌트의 실제 경로에 맞게 수정하세요
 import SignUp from './pages/auth/signup/SignUp';
+import RecipeDetail from './pages/recipedetail/RecipeDetail';
 
 function App() {
 	return (
 		<>
-			<Login />
-			<SignUp />
+			<Routes>
+				<Route path="/" element="" />
+				<Route path="/login" element={<Login />} />
+				<Route path="/signup" element={<SignUp />} />
+				<Route path="/recipedetail" element={<RecipeDetail />} />
+			</Routes>
 		</>
 	);
 }
