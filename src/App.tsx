85c0399307a9a8f3590f9064e1acae98d7f1f87d
박초롱 <rong9835@naver.com>
2024-10-08 10:10:28 +0900

    Design 로그인css작업

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import { useState } from 'react';
 import './App.css';
-
-import Login from './pages/auth/login/Login'; // Login 컴포넌트의 실제 경로에 맞게 수정하세요
+import Login from './pages/login/Login'; // Login 컴포넌트의 실제 경로에 맞게 수정
 import SignUp from './pages/auth/signup/SignUp';
 
 function App() {
