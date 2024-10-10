d596cf3fcd6a7b0fd6a81c058102f47f834b8513
박초롱 <rong9835@naver.com>
2024-10-10 16:12:50 +0900

    pr충돌해결

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
 import { useState } from 'react';
 import './App.css';
-import Login from './pages/login/Login'; // Login 컴포넌트의 실제 경로에 맞게 수정
+import Login from './pages/login/Login'; 
 import SignUp from './pages/auth/signup/SignUp';
 
 function App() {
   return (
     <>
       <Login />
-      {/* <SignUp /> */}
+      <SignUp />
     </>
   );
 }
