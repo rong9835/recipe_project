2e96467f3896efbae72bcb9b2d6706ab59b62d75
Hongaproject <tkwlscjq99@gmail.com>
2024-10-10 14:04:50 +0900

    feat: 프로필페이지 기본 틀 제작

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import { useState } from 'react';
 import './App.css';
 import Login from './pages/auth/login/Login'; // Login 컴포넌트의 실제 경로에 맞게 수정하세요
 import SignUp from './pages/auth/signup/SignUp';
+import Profile from './pages/auth/profile/Profile';
 
 function App() {
 	return (
 		<>
 			<Login />
 			<SignUp />
+			<Profile />
 		</>
 	);
 }
