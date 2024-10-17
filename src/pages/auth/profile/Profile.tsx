650adaf30dbb9a4f42f7e1ae272d054a67d9be98
Hongaproject <tkwlscjq99@gmail.com>
2024-10-17 15:28:35 +0900

    업데이트 하기 위해 임시 저장

--- src/pages/auth/profile/Profile.tsx
+++ src/pages/auth/profile/Profile.tsx
@@ -1,35 +1,51 @@
+import { useEffect, useState } from 'react';
 import styles from './Profile.module.css';
+import { auth } from '../../../firebase/config';
 
 export default function Profile() {
+	
+	const [userName, setUserName] = useState<string | null>(null);
+
+	useEffect(() => {
+		const user = auth.currentUser;
+		if(user){
+			setUserName(user.displayName);
+		} else {
+			setUserName("Unknown User");
+		}
+	})
 	return (
-		<main>
-			<h1>마이페이지</h1>
-			<section>
-				<div>
+		<main className={styles.container}>
+			<div className={styles.logo}>
+				<img src="./src/assets/icon_mypage.png" alt="" />
+				<h1>마이페이지</h1>
+			</div>
+			<section className={styles.profile}>
+				<div className={styles.user}>
 					<div>이미지 부분</div>
-					<span>회원이름</span>
+					<span>{userName}</span>
 					<span>회원소개</span>
 				</div>
-				<div>
+				<div className={styles.userPost}>
 					<div>이미지 부분</div>
 					<span>등록된 게시물</span>
 					<span>갯수</span>
 				</div>
-				<div>
+				<div className={styles.userClass}>
 					<div>이미지 부분</div>
 					<span>뱃지 등급</span>
 					<span>갯수</span>
 				</div>
 			</section>
-			<section>
-				<div>
+			<section className={styles.userInformation}>
+				<div className={styles.userRecipe}>
 					<div>레시피 | Recipe</div>
 					<div>
 						<button>등록한 레시피</button>
 						<button>좋아요 누른 레시피</button>
 					</div>
 				</div>
-				<div>
+				<div className={styles.userInfo}>
 					<div>회원정보 | User Info</div>
 					<div>
 						<button>정보수정</button>
