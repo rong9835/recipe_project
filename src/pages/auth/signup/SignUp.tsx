0540beb532b1ded455dc243d16601a132d92a3f2
Forhye <onlyh4425@gmail.com>
2024-10-20 23:48:53 +0900

    fix : 중복 체크 안되던 오류 해결 - 데이터베이스 조회시 필드명과 일치하지 않아서 중복체크가 되지 않았던 문제 해결 - 필드명을 db와 일치하도록 user_ 추가

--- src/pages/auth/signup/SignUp.tsx
+++ src/pages/auth/signup/SignUp.tsx
@@ -4,7 +4,14 @@ import { FirebaseError } from 'firebase/app';
 import { createUserWithEmailAndPassword } from 'firebase/auth';
 import { auth, db } from '../../../firebase/config';
 import { Link, useNavigate } from 'react-router-dom';
-import { collection, doc, getDocs, query, setDoc, where } from 'firebase/firestore';
+import {
+	collection,
+	doc,
+	getDocs,
+	query,
+	setDoc,
+	where,
+} from 'firebase/firestore';
 
 interface SignUpValues {
 	email: string;
@@ -16,23 +23,24 @@ interface SignUpValues {
 }
 
 export default function SignUp() {
+	const [signUpValues, setSignUpValues] = useState<SignUpValues>({
+		email: '',
+		password: '',
+		confirmPassword: '',
+		name: '',
+		nickname: '',
+		phone: '',
+	});
+	const [isLoading, setIsLoading] = useState<boolean>(false);
+	const [errMsg, setErrMsg] = useState<string>('');
+	const [emailAvailable, setEmailAvailable] = useState<boolean | null>(null);
+	const [nicknameAvailable, setNicknameAvailable] = useState<boolean | null>(
+		null
+	);
+	const [phoneAvailable, setPhoneAvailable] = useState<boolean | null>(null);
+	const navigate = useNavigate();
 
-    const [signUpValues, setSignUpValues] = useState<SignUpValues>({
-        email: '',
-        password: '',
-        confirmPassword: '',
-        name:'',
-        nickname:'',
-        phone:''
-    });
-    const [isLoading, setIsLoading] = useState<boolean>(false);
-    const [errMsg, setErrMsg] = useState<string>("");
-    const [emailAvailable, setEmailAvailable] = useState<boolean | null>(null);
-    const [nicknameAvailable, setNicknameAvailable] = useState<boolean | null>(null);
-    const [phoneAvailable, setPhoneAvailable] = useState<boolean | null>(null);
-    const navigate = useNavigate();
-
-    // 입력 값 받는 함수
+	// 입력 값 받는 함수
 	const signUpInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
 		const { name, value } = e.target;
 		setSignUpValues({
@@ -41,121 +49,182 @@ export default function SignUp() {
 		});
 	};
 
-    // 중복 체크 함수
-    const checkEmailAvailability = async () => {
-        const q = query(collection(db, 'users'), where('email', '==', signUpValues.email));
-        const querySnapshot = await getDocs(q);
-        setEmailAvailable(querySnapshot.empty);
-    }
+	// 중복 체크 함수
+	const checkEmailAvailability = async () => {
+		const q = query(
+			collection(db, 'users'),
+			where('email', '==', signUpValues.email)
+		);
+		const querySnapshot = await getDocs(q);
+		setEmailAvailable(querySnapshot.empty);
+	};
 
-    const checkNicknameAvailability = async () => {
-        const q = query(collection(db, 'users'), where('nickname', '==', signUpValues.nickname));
-        const querySnapshot = await getDocs(q);
-        setNicknameAvailable(querySnapshot.empty); 
-    };
+	const checkNicknameAvailability = async () => {
+		const q = query(
+			collection(db, 'users'),
+			where('user_nickname', '==', signUpValues.nickname)
+		);
+		const querySnapshot = await getDocs(q);
+		setNicknameAvailable(querySnapshot.empty);
+	};
 
-    const checkPhoneAvailability = async () => {
-        const q = query(collection(db, 'users'), where('phone', '==', signUpValues.phone));
-        const querySnapshot = await getDocs(q);
-        setPhoneAvailable(querySnapshot.empty); 
-    };
+	const checkPhoneAvailability = async () => {
+		const q = query(
+			collection(db, 'users'),
+			where('user_phone_number', '==', signUpValues.phone)
+		);
+		const querySnapshot = await getDocs(q);
+		setPhoneAvailable(querySnapshot.empty);
+	};
 
+	// 회원가입 폼 제출 함수
+	const signUpFormSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+		e.preventDefault();
+		setErrMsg('');
 
-    // 회원가입 폼 제출 함수
-    const signUpFormSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
-        e.preventDefault();
-        setErrMsg("");        
-        
-        if (isLoading || 
-            signUpValues.email === "" || 
-            signUpValues.password === "" || 
-            signUpValues.confirmPassword === "" || 
-            signUpValues.name === "" || 
-            signUpValues.nickname === "" || 
-            signUpValues.phone === "") { 
-            return;
-        }
+		if (
+			isLoading ||
+			signUpValues.email === '' ||
+			signUpValues.password === '' ||
+			signUpValues.confirmPassword === '' ||
+			signUpValues.name === '' ||
+			signUpValues.nickname === '' ||
+			signUpValues.phone === ''
+		) {
+			return;
+		}
 
-        // 일치, 중복 여부 체크
-        if (signUpValues.password !== signUpValues.confirmPassword) {
-            setErrMsg("비밀번호가 일치하지 않습니다.");
-            return;
-        }
+		// 일치, 중복 여부 체크
+		if (signUpValues.password !== signUpValues.confirmPassword) {
+			setErrMsg('비밀번호가 일치하지 않습니다.');
+			return;
+		}
 
-        if(!emailAvailable){
-            setErrMsg("이미 사용 중인 이메일입니다.");
-            return;
-        }
+		if (!emailAvailable) {
+			setErrMsg('이미 사용 중인 이메일입니다.');
+			return;
+		}
 
-        if (!nicknameAvailable){
-            setErrMsg("이미 사용 중인 닉네임입니다.");
-            return;
-        }
+		if (!nicknameAvailable) {
+			setErrMsg('이미 사용 중인 닉네임입니다.');
+			return;
+		}
 
-        if(!phoneAvailable){
-            setErrMsg("이미 사용 중인 번호입니다.");
-            return;
-        }
+		if (!phoneAvailable) {
+			setErrMsg('이미 사용 중인 번호입니다.');
+			return;
+		}
 
-        try {
-            setIsLoading(true);
-            // Firebase에서 사용자 계정 생성
-            const userCreate = await createUserWithEmailAndPassword(
-                auth,
-                signUpValues.email,
-                signUpValues.password
-            );
+		try {
+			setIsLoading(true);
+			// Firebase에서 사용자 계정 생성
+			const userCreate = await createUserWithEmailAndPassword(
+				auth,
+				signUpValues.email,
+				signUpValues.password
+			);
 
-            // Firestore에 사용자 정보 저장하기
-            const userDocRef = doc(db, 'users', userCreate.user.uid); 
-            await setDoc(userDocRef, { 
-                recipes_total: 0,
-                user_email: signUpValues.email,
-                user_info: "",
-                user_name: signUpValues.name,
-                user_nickname: signUpValues.nickname,
-                user_phone_number: signUpValues.phone,
-            });
+			// Firestore에 사용자 정보 저장하기
+			const userDocRef = doc(db, 'users', userCreate.user.uid);
+			await setDoc(userDocRef, {
+				recipes_total: 0,
+				user_email: signUpValues.email,
+				user_info: '',
+				user_name: signUpValues.name,
+				user_nickname: signUpValues.nickname,
+				user_phone_number: signUpValues.phone,
+				provider: 'normal',
+			});
 
-            console.log("회원가입 성공:", userCreate.user);
-            alert("회원가입이 되었습니다.");
+			console.log('회원가입 성공:', userCreate.user);
+			alert('회원가입이 되었습니다.');
 
-            navigate('/login');
+			navigate('/login');
+		} catch (e) {
+			if (e instanceof FirebaseError) {
+				setErrMsg(e.message);
+			}
+		} finally {
+			setIsLoading(false); // 로딩 상태 해제
+		}
+	};
 
-        } catch (e) {
-            if (e instanceof FirebaseError) {
-                setErrMsg(e.message); 
-            }
-        } finally {
-            setIsLoading(false); // 로딩 상태 해제
-        }
-    };
-    
-    return (
-        <main className={styles.container}>
-            <div className={styles.logo}>
-                <Link to={'/'}>
-                    <img src="./src/assets/icon_logo.png" alt="" />
-                </Link>
-            </div>
-            <form onSubmit={signUpFormSubmit}>
-                <input type="email" name="email" value={signUpValues.email} onChange={signUpInputChange} onBlur={checkEmailAvailability} placeholder="이메일 형식으로 입력해 주세요." required className={styles.input}/>
-                {emailAvailable === false && <div>{errMsg}</div>}
-                <input type="password" name="password" value={signUpValues.password} onChange={signUpInputChange} placeholder="비밀번호 6자리 이상 입력해 주세요." required className={styles.input}/>
-                <input type="password" name="confirmPassword" value={signUpValues.confirmPassword} onChange={signUpInputChange} placeholder="비밀번호 확인" required className={styles.input}/>
-                <input type="text" name="name" value={signUpValues.name} onChange={signUpInputChange} placeholder="이름을 입력해 주세요." required className={styles.input}/>
-                <input type="text" name="nickname" value={signUpValues.nickname} onChange={signUpInputChange} onBlur={checkNicknameAvailability} placeholder="닉네임을 입력해 주세요." required className={styles.input}/>
-                {nicknameAvailable === false && <div>{errMsg}</div>}
-                <input type="tel" name="phone" value={signUpValues.phone} onChange={signUpInputChange} onBlur={checkPhoneAvailability} placeholder="연락처를 입력해 주세요." required className={styles.input}/>
-                {phoneAvailable === false && <div>{errMsg}</div>}
-                {errMsg && <div>{errMsg}</div>}
-                <button type="submit" disabled={isLoading} className={styles.signupBtn}>회원가입</button> 
-            </form>
-            <div className={styles.pageLogin}>
-                <Link to={'/login'}>
-                    로그인하기
-                </Link>
-            </div>
-        </main>
-    );
+	return (
+		<main className={styles.container}>
+			<div className={styles.logo}>
+				<Link to={'/'}>
+					<img src="./src/assets/icon_logo.png" alt="" />
+				</Link>
+			</div>
+			<form onSubmit={signUpFormSubmit}>
+				<input
+					type="email"
+					name="email"
+					value={signUpValues.email}
+					onChange={signUpInputChange}
+					onBlur={checkEmailAvailability}
+					placeholder="이메일 형식으로 입력해 주세요."
+					required
+					className={styles.input}
+				/>
+				{emailAvailable === false && <div>{errMsg}</div>}
+				<input
+					type="password"
+					name="password"
+					value={signUpValues.password}
+					onChange={signUpInputChange}
+					placeholder="비밀번호 6자리 이상 입력해 주세요."
+					required
+					className={styles.input}
+				/>
+				<input
+					type="password"
+					name="confirmPassword"
+					value={signUpValues.confirmPassword}
+					onChange={signUpInputChange}
+					placeholder="비밀번호 확인"
+					required
+					className={styles.input}
+				/>
+				<input
+					type="text"
+					name="name"
+					value={signUpValues.name}
+					onChange={signUpInputChange}
+					placeholder="이름을 입력해 주세요."
+					required
+					className={styles.input}
+				/>
+				<input
+					type="text"
+					name="nickname"
+					value={signUpValues.nickname}
+					onChange={signUpInputChange}
+					onBlur={checkNicknameAvailability}
+					placeholder="닉네임을 입력해 주세요."
+					required
+					className={styles.input}
+				/>
+				{nicknameAvailable === false && <div>{errMsg}</div>}
+				<input
+					type="tel"
+					name="phone"
+					value={signUpValues.phone}
+					onChange={signUpInputChange}
+					onBlur={checkPhoneAvailability}
+					placeholder="연락처를 입력해 주세요."
+					required
+					className={styles.input}
+				/>
+				{phoneAvailable === false && <div>{errMsg}</div>}
+				{errMsg && <div>{errMsg}</div>}
+				<button type="submit" disabled={isLoading} className={styles.signupBtn}>
+					회원가입
+				</button>
+			</form>
+			<div className={styles.pageLogin}>
+				<Link to={'/login'}>로그인하기</Link>
+			</div>
+		</main>
+	);
 }
