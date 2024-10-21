a950ce806401ab23cc786071edc94f2212233b18
Forhye <onlyh4425@gmail.com>
2024-10-22 00:22:42 +0900

    rename : 멤버 등급 스푼 이미지 깨짐 현상으로 인한 파일 수정

--- src/pages/auth/profile/spoon/Spoon.tsx
+++ src/pages/auth/profile/spoon/Spoon.tsx
@@ -9,11 +9,7 @@ export default function Spoon({ onClose }: SpoonProps) {
 		<main className={styles.container}>
 			<div className={styles.modalContent}>
 				<div className={styles.header}>
-					<img
-						src="/assets/icon_logoSpoon.png"
-						alt="스푼 로고이미지"
-						className={styles.logo}
-					/>
+					<p className={styles.logo}>Member Grade Bedge</p>
 					<img
 						src="/assets/icon_close.png"
 						alt="닫기 버튼"
@@ -24,31 +20,31 @@ export default function Spoon({ onClose }: SpoonProps) {
 				<h1 className={styles.title}>회원 등급별 뱃지</h1>
 				<div className={styles.spoonBox}>
 					<div className={styles.spoon1}>
-						<img src="/assets/icon_spoon.png" alt="" />
+						<img src="/assets/step_1_spoon.svg" alt="" />
 						<h3>스푼 Spoon</h3>
 						<span>게시물</span>
 						<p>0개</p>
 					</div>
 					<div className={styles.spoon1}>
-						<img src="/assets/icon_spoon2.png" alt="" />
+						<img src="/assets/step_2_fork.svg" alt="" />
 						<h3>포크 Fork</h3>
 						<span>게시물</span>
 						<p>10개 등록 시</p>
 					</div>
 					<div className={styles.spoon1}>
-						<img src="/assets/icon_spoon3.png" alt="" />
+						<img src="/assets/step_3_chops.svg" alt="" />
 						<h3>챕스 Chopsticks</h3>
 						<span>게시물</span>
 						<p>20개 등록 시</p>
 					</div>
 					<div className={styles.spoon1}>
-						<img src="/assets/icon_spoon4.png" alt="" />
+						<img src="/assets/step_4_turner.svg" alt="" />
 						<h3>터너 Turner</h3>
 						<span>게시물</span>
 						<p>30개 등록 시</p>
 					</div>
 					<div className={styles.spoon1}>
-						<img src="/assets/icon_spoon5.png" alt="" />
+						<img src="/assets/step_5_knif.svg" alt="" />
 						<h3>나이프 Knif</h3>
 						<span>게시물</span>
 						<p>40개 등록 시</p>
